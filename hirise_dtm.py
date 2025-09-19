5b473361b364a7364f75a2ff36c1b473689938f3
GianM0027 <104518262+GianM0027@users.noreply.github.com>
2025-09-19 19:31:09 +0200

    Structure of  new FOV methods

--- hirise_dtm.py
+++ hirise_dtm.py
@@ -1,4 +1,6 @@
 import os
+
+import numpy as np
 import rasterio
 from typing import Tuple, Dict
 from matplotlib import pyplot as plt
@@ -8,6 +10,8 @@ class HiriseDTM:
     """
     This class takes as input the path to a local HiRISE .IMG file, converts it into a NumPy array,
     and provides a set of utility functions for working with it.
+
+    :param img_path: Path to a local HiRISE .IMG file.
     """
 
     def __init__(self, img_path: str | os.PathLike):
@@ -20,22 +24,35 @@ class HiriseDTM:
             data[data == nodata] = float("nan")
 
         self.img_path = img_path
-        self.image = data
+        self.numpy_image = data
         self.file_name = os.path.split(img_path)[-1].replace(".IMG", "")
         self.metadata = self._get_metadata()
 
+    def get_portion_of_map(self, size):
+        # todo: metodo che estrae una sezione dell'immagine di grandezza (size x size), evitando i NaN
+        # todo: il metodo restituisce anche le coordinate top-left dalle quali è stata ricavata la porzione di mappa
+        #       IMPORTANTE: la top left position è una tupla (x, y) dove x è la colonna e y è la riga
 
-    # todo: metodo che estrae una sezione dell'immagine di grandezza fissa
-    # todo: metodo che restituisce le dimensioni dell'immagine
-    # todo: migliorare la funzione di plot per essere più nitida, ma anche per plottare una sezione della mappa
+        return np.zeros((size, size)), (0, 0)
+
+    def get_field_of_view(self, position, fov_distance):
+        # todo: metodo che dato un punto di coordinate (x,y) restituisce la matrice booleana che evidenzia i pixel che
+        #       il rover vede da quella posizione
+
+        return np.ones((fov_distance, fov_distance))
+
+    def get_lowest_highest_altitude(self):
+        return np.nanmin(self.numpy_image), np.nanmax(self.numpy_image)
 
     def plot_dtm(self, figsize: Tuple = (12, 12)) -> None:
         """
-        :param figsize: plot figure size.
-        Shows the DTM image in a matplotlib figure.
+        :param figsize: plot figure map_size.
+        Shows the DTM numpy_image in a matplotlib figure.
         """
+        # todo: migliorare la funzione di plot per essere più nitida, ma anche per plottare una sezione della mappa invece
+        #       della mappa intera
         plt.figure(figsize=figsize)
-        plt.imshow(self.image, cmap="terrain")
+        plt.imshow(self.numpy_image, cmap="terrain")
         plt.colorbar(label="Elevation (m)")
         plt.title("HiRISE DTM")
         plt.show()
@@ -59,16 +76,16 @@ class HiriseDTM:
         projection = "Equirectangular" if aabcd[3] == "E" else "Polar Stereographic" if aabcd[3] == "P" else unk
 
         grid_spacing = 0.25 if aabcd[4] == "A" else \
-            0.5 if aabcd[4] == "B" else \
-                1.0 if aabcd[4] == "C" else \
-                    2 if aabcd[4] == "D" else unk
+                       0.5 if aabcd[4] == "B" else \
+                       1.0 if aabcd[4] == "C" else \
+                       2 if aabcd[4] == "D" else unk
 
         producing_institution = "USGS" if Vnn[0] == "U" else \
-            "University of Arizona" if Vnn[0] == "A" else \
-                "CalTech" if Vnn[0] == "C" else \
-                    "NASA Ames" if Vnn[0] == "N" else \
-                        "JPL" if Vnn[0] == "J" else \
-                            "Ohio State" if Vnn[0] == "O" else \
+                                "University of Arizona" if Vnn[0] == "A" else \
+                                "CalTech" if Vnn[0] == "C" else \
+                                "NASA Ames" if Vnn[0] == "N" else \
+                                "JPL" if Vnn[0] == "J" else \
+                                "Ohio State" if Vnn[0] == "O" else \
                                 "Planetary Science Institute" if Vnn[0] == "P" else unk
 
         metadata = {"product_type": product_type,
