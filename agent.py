2d59e2a30076b30b14ea92f21fb1f54c8bfee57b
GianM0027 <104518262+GianM0027@users.noreply.github.com>
2025-09-24 21:36:01 +0200

    agent class creation

---
+++ agent.py
@@ -0,0 +1,5 @@
+#todo classe agent, che si occupa della logica di apprendimento, dell'interazione con l'ambiente e dell'aggiornamento della politica.
+
+class Agent():
+    def __init__(self):
+        pass
