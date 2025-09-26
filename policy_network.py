f49817e2dd302f85add5b24235f624d5613a1956
GianM0027 <104518262+GianM0027@users.noreply.github.com>
2025-09-26 09:24:32 +0200

    todo update

--- policy_network.py
+++ policy_network.py
@@ -5,8 +5,16 @@ class policy_network(torch.nn.Module):
     def __init__(self, n_actions=8):
         super(policy_network, self).__init__()
 
-        # todo: implementare il modello che prende in input una batch di observations (come organizzato in agent.py)
-        #       e restituisce una di X possibili azioni (+ value se necessario)
+        # todo: implementare la CNN con le seguenti restrizioni:
+        #       .
+        #       INPUT:
+        #       - Deve prendere in input due matrici quadrate con shape (n, n). Anche fuse assieme in una matrice (n, n, 2)
+        #       - Due array con shape (2,). Queste sono le posizioni del rover e del target.
+        #       .
+        #       OUTPUT:
+        #       - Una delle 8 possibili azioni (0-7)
+        #       - Un float restituito come logit. Questo rappresenta il "vantaggio" di usare l'azione restituita dall'altra testa.
+
 
     def forward(self, x):
         pass
