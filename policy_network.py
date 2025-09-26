171201186923c7722e677599258b0e9269d68c12
GianM0027 <104518262+GianM0027@users.noreply.github.com>
2025-09-26 13:09:52 +0200

    todo update

--- policy_network.py
+++ policy_network.py
@@ -8,19 +8,26 @@ class policy_network(torch.nn.Module):
         # todo: implementare la CNN con le seguenti restrizioni:
         #       .
         #       INPUT:
-        #       - Deve prendere in input due matrici quadrate con shape (n, n). Anche fuse assieme in una matrice (n, n, 2)
-        #       - Due array con shape (2,). Queste sono le posizioni del rover e del target.
+        #       - Deve prendere in input una matrice con shape (3, n, n).
         #       .
         #       OUTPUT:
         #       - Una delle 8 possibili azioni (0-7)
         #       - Un float restituito come logit. Questo rappresenta il "vantaggio" di usare l'azione restituita dall'altra testa.
 
+    def preprocess(self, x):
+        # todo:
+        #   - La normalizzazione delle altimetrie (canale 0) va fatta con minmax (ignorando i np.NaN)
+        #   - La normalizzazione del counter (canale 1) va fatto con log(1 + x)
+        #   - La posizione del rover e del target (canale 2) non va normalizzata
+        pass
 
     def forward(self, args):
         pass
 
-    def fit(self, batch, minibatch_size, loss, epochs_per_minibatch=1):
-        #todo: dati i parametri della funzione, esegui l'aggiornamento dei pesi della rete poi resituisci il valore delle loss per ogni minibatch
+    def fit(self, minibatch, loss, epochs_per_minibatch=1):
+        # todo: dati i parametri della funzione, esegui l'aggiornamento dei pesi della rete usando un unico minibatch,
+        #      poi resituisci il valore delle loss per ogni minibatch
+
         loss_values = []
         return loss_values
 
