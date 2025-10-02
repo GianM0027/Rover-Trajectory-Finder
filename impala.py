6af130612d3f310a41e5ed0d73910fdb8ae7219e
GianM0027 <104518262+GianM0027@users.noreply.github.com>
2025-10-02 10:48:46 +0200

    Impala-like CNN

---
+++ impala.py
@@ -0,0 +1,98 @@
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+import torch.nn.functional as F
+
+
+def initialize_weights(m):
+    if isinstance(m, nn.Conv2d):
+        init.xavier_normal_(m.weight)
+        if m.bias is not None:
+            init.constant_(m.bias, 0)
+    elif isinstance(m, nn.Linear):
+        if m.out_features == 15:
+            # Initialize weights 100 times smaller than usual
+            std = init.calculate_gain('linear') / 100
+            init.normal_(m.weight, mean=0.0, std=std)
+            init.constant_(m.bias, 0)
+        else:
+            init.xavier_normal_(m.weight)
+            init.constant_(m.bias, 0)
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, filters):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(filters, filters, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(filters)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(filters, filters, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(filters)
+
+    def forward(self, x):
+        residual = x
+        out = self.bn1(self.conv1(x))
+        out = self.relu(out)
+        out = self.bn2(self.conv2(out))
+        out += residual
+        out = self.relu(out)
+        return out
+
+
+class ConvolutionalBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ConvolutionalBlock, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.resblock1 = ResidualBlock(out_channels)
+        self.resblock2 = ResidualBlock(out_channels)
+
+    def forward(self, x):
+        x = self.bn1(self.conv1(x))
+        x = self.pool(x)
+        x = self.resblock1(x)
+        x = self.resblock2(x)
+        return x
+
+
+class ImpalaModel(nn.Module):
+    def __init__(self, action_space=8):
+        super(ImpalaModel, self).__init__()
+
+        self.conv_block1 = ConvolutionalBlock(4, 16)
+        self.conv_block2 = ConvolutionalBlock(16, 32)
+        self.conv_block3 = ConvolutionalBlock(32, 32)
+
+        # Remaining parts of the model
+        self.feed_forward = nn.Linear(128, 64)
+
+        self.policy_head = nn.Linear(64, action_space)
+        self.value_head = nn.Linear(64, 1)
+        self.softmax_activation = nn.Softmax(dim=1)
+
+        self.apply(initialize_weights)
+
+    def forward(self, x):
+        x = self.conv_block1(x)
+        x = self.conv_block2(x)
+        x = self.conv_block3(x)
+
+        x = F.relu(x)
+        x = x.reshape(x.size(0), -1)
+        x = self.feed_forward(x)
+        x = F.relu(x)
+
+        policy = self.softmax_activation(self.policy_head(x))
+        value = self.value_head(x)
+
+        return policy, value
+
+    def save_weights(self, path: str):
+        torch.save(self.state_dict(), path)
+
+    def load_weights(self, path: str, device: str = "cuda"):
+        state_dict = torch.load(path, map_location=device)
+        self.load_state_dict(state_dict)
+        self.eval()
--- main.py
+++ main.py
@@ -15,66 +15,61 @@ else:
     device = torch.device('cpu')
 print(f"Using device: {device}")
 
-# todo: data augmentation sulle porzioni della mappa
+
+# todo: dare una ripulita al codice qua, farlo più comprensibile
 
 TRAINING_SEED = 42
-VALIDATION_SEED = 42*10**20
+
 np.random.seed(TRAINING_SEED)
 weights_path = os.path.join('weights', 'weights.h5')
 config = {
-           "input_channels": 3,
-           "backbone": [
-               {"type": "conv", "out_channels": 32, "kernel_size": 3, "stride": 1, "padding": 1, "activation": "relu"},
-               {"type": "pool", "mode": "max", "kernel_size": 2},
-               {"type": "conv", "out_channels": 64, "kernel_size": 3, "stride": 1, "padding": 1, "activation": "relu"},
-               {"type": "pool", "mode": "max", "kernel_size": 2}
-           ],
-           "head_action": [
-               {"type": "fc", "out_features": 64, "activation": "relu"},
-               {"type": "fc", "out_features": 8}
-           ],
-           "head_value": [
-               {"type": "fc", "out_features": 64, "activation": "relu"},
-               {"type": "fc", "out_features": 1}
-           ]
-       }
+   "input_channels": 3,
+   "backbone": [
+       {"type": "conv", "out_channels": 32, "kernel_size": 3, "stride": 1, "padding": 1, "activation": "relu"},
+       {"type": "pool", "mode": "max", "kernel_size": 2},
+       {"type": "conv", "out_channels": 64, "kernel_size": 3, "stride": 1, "padding": 1, "activation": "relu"},
+       {"type": "pool", "mode": "max", "kernel_size": 2}
+   ],
+   "head_action": [
+       {"type": "fc", "out_features": 64, "activation": "relu"},
+       {"type": "fc", "out_features": 8}
+   ],
+   "head_value": [
+       {"type": "fc", "out_features": 64, "activation": "relu"},
+       {"type": "fc", "out_features": 1}
+   ]
+}
 
 filepath = "DTMs/DTEEC_016460_2230_016170_2230_G01.IMG"
 dtm_file = HiriseDTM(filepath)
-TRAIN = False
+
+TRAIN = True
 map_size = 15
 fov_distance = 3
 
-if TRAIN:
-    render_mode = "rgb_array"
-else:
-    render_mode = "human"
 grid_mars_env = GridMarsEnv(dtm_file,
-                            render_mode=render_mode,
+                            render_mode="rgb_array" if TRAIN else "human",
                             map_size=map_size,
                             fov_distance=fov_distance,
                             rover_max_step=1,
                             rover_max_drop=1,
                             rover_max_number_of_steps=200)
 
-policy_network = PolicyNetwork(config)
-#policy_network = ImpalaModel()
-
-if not TRAIN:
-    policy_network(torch.randn(1, 3, map_size, map_size))
-    policy_network.load_weights(weights_path)
-
+# policy_network = PolicyNetwork(config)
+policy_network = ImpalaModel()
 
 if TRAIN:
     agent = Agent(mars_environment=grid_mars_env, policy_network=policy_network, seed=TRAINING_SEED)
-    agent.train(training_episodes=20000,
+    agent.train(training_episodes=1000,
                 batch_size=512,
-                minibatch_size=64,
-                epochs=1,
+                minibatch_size=128,
+                epochs=2,
                 weights_path=weights_path,
                 device=device,
                 step_verbose=True,
-                c2=0.03)
+                c2=0.01)
 else:
-    agent = Agent(mars_environment=grid_mars_env, policy_network=policy_network, seed=VALIDATION_SEED)
+    policy_network(torch.randn(1, 4, map_size, map_size))
+    policy_network.load_weights(weights_path)
+    agent = Agent(mars_environment=grid_mars_env, policy_network=policy_network)
     agent.run_simulation(use_policy_network=True, device=device, verbose=True, sample_action=True)
